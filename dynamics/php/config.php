bcbec5399f39fd92df5b3665cfee617902772f9d
jaasrl <riosjaasiel@comunidad.unam.mx>
2020-06-24 11:22:29 -0500

    FEAT:Agregue el archivo de configuarcion para poder usar la BD p6_opina

---
+++ dynamics/php/config.php
@@ -0,0 +1,21 @@
+<?php
+  $base="p6_opina";
+  define("DBUSER","Administrador");
+  define("DBHOST","localhost");
+  define("PASSWORD",'c0Y0op1n10nE$pR3p@6&');
+  function connect () {
+    return mysqli_connect(DBHOST, DBUSER, PASSWORD);
+  }
+  function connectDB1 ($conexion, $base) {
+    return mysqli_select_db($conexion, $base)
+    or die("F. No pude entrar a la base");
+  }
+  function connectDB2 ($base) {
+    $con = mysqli_connect(DBHOST, DBUSER, PASSWORD, $base);
+    if (!$con)
+    {
+      echo "No se ha podido acceder a la base. <br>";
+    }
+    return $con;
+  }
+?>
